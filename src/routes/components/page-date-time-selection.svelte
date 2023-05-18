7cc2c175676c3776cb0c2995bec66b0562a06493
Anton Shubin <2spy4x@gmail.com>
2023-05-18 15:02:14 +0700

    feat: date and time selection page logic

--- src/routes/components/page-date-time-selection.svelte
+++ src/routes/components/page-date-time-selection.svelte
@@ -1,13 +1,56 @@
 <script lang="ts">
-	import { store, Page } from '../state';
+	import { store, Page, Day } from '../state';
+
+	function capitalize(str: string) {
+		return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+	}
 </script>
 
 <h1 class="text-2xl font-bold mb-10">Page 3 (date time selection)</h1>
+
+<!-- Day selector -->
+<select bind:value={$store.day}>
+	{#each Object.keys(Day) as day}
+		<option value={day}>{capitalize(day)}</option>
+	{/each}
+</select>
+
+<!-- AM/PM checkbox -->
+<label class="flex gap-2">
+	<input type="checkbox" bind:checked={$store.isAM} class="w-10 h-10" />
+	<div>
+		<p>AM</p>
+		<p>PM</p>
+	</div>
+</label>
+
+<!-- Hour selector -->
+<select bind:value={$store.hour}>
+	{#each Array.from(Array(12).keys()) as hour}
+		<option value={hour + 1}>{hour + 1}</option>
+	{/each}
+</select>
+
+<!-- Minute selector -->
+<select bind:value={$store.minute}>
+	{#each Array.from(Array(60).keys()) as minute}
+		<option value={minute}>{minute}</option>
+	{/each}
+</select>
+
+<!-- shouldNotifyDayBefore checkbox -->
+<label class="flex gap-2">
+	<input type="checkbox" bind:checked={$store.shouldNotifyDayBefore} class="w-10 h-10" />
+	<div>
+		<p>I would like to be informed the day before</p>
+	</div>
+</label>
+
 <div class="flex gap-3">
-	<button class="btn btn-secondary" on:click={() => store.setPage(Page.PRIVACY_POLICY_CONSENT)}
-		>Back</button
-	>
-	<button class="btn btn-primary" on:click={() => store.setPage(Page.DOWNLOAD_CALENDAR_FILE)}
-		>Confirm</button
-	>
+	<button class="btn btn-secondary" on:click={() => store.setPage(Page.PRIVACY_POLICY_CONSENT)}>
+		Back
+	</button>
+	<button class="btn btn-primary" on:click={() => store.setPage(Page.DOWNLOAD_CALENDAR_FILE)}>
+		Confirm
+	</button>
 </div>
--- src/routes/state.ts
+++ src/routes/state.ts
@@ -28,14 +28,34 @@ export const DoseDetails = {
 	}
 };
 
+export enum Day {
+	MONDAY = 'MONDAY',
+	TUESDAY = 'TUESDAY',
+	WEDNESDAY = 'WEDNESDAY',
+	THURSDAY = 'THURSDAY',
+	FRIDAY = 'FRIDAY',
+	SATURDAY = 'SATURDAY',
+	SUNDAY = 'SUNDAY'
+}
+
 interface State {
 	page: Page;
 	dose: Dose;
+	day: Day;
+	hour: number;
+	minute: number;
+	isAM: boolean;
+	shouldNotifyDayBefore: boolean;
 }
 
 const initialState: State = {
 	page: Page.DOSE_SELECTION,
-	dose: Dose.D_10_MG
+	dose: Dose.D_10_MG,
+	day: Day.MONDAY,
+	hour: 8,
+	minute: 0,
+	isAM: true,
+	shouldNotifyDayBefore: true
 };
 
 const _store = writable<State>(initialState);
@@ -44,7 +64,12 @@ function mutateState(mutation: Partial<State>) {
 }
 
 export const store = {
-	subscribe: _store.subscribe,
-	setPage: (page: Page) => mutateState({ page }),
-	setDose: (dose: Dose | string) => mutateState({ dose: dose as Dose })
+	..._store,
+	setPage: (page: Page) => {
+		// TODO: report event to Google Analytics
+		mutateState({ page });
+	},
+	setDose: (dose: Dose | string) => {
+		mutateState({ dose: dose as Dose });
+	}
 };
